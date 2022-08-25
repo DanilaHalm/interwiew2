ac3cd2322a6955e32336d67f81478afbe477fb0c
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2022-08-25 10:21:36 +0300

    Update index.js
    Updt

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+var a = "danila" 
 
+console.log(a + b);
+
+var b = "nastya"
+
+---------- 
+
+var a = "danila" 
+var b
+console.log(a + b);
+
+b = "nastya"
+---------- 
+let a = "danila" 
+
+console.log(a + b);
+
+let b = "nastya"
+---------- 
+let a = "danila" 
+let b
+console.log(a + b);
+
+b = "nastya"
+----------
