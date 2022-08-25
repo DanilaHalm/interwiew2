9b7ebea390b1e26b52e899f5edb59afc98fc49d3
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2022-08-25 11:38:35 +0300

    Update index.js
    Updt

--- index.js
+++ index.js
@@ -35,9 +35,21 @@ let testObj = {
     king: "danila",
     queen: "nastya"
 } 
+
 Object.keys(testObj) // => 
+
 Object.values(testObj) // => 
+
 let entries = Object.entries(testObj) // => 
 
+let fromEntries = Object.fromEntries(entries) // => 
+
+let fooTestObj = Object.freeze(testObj)
+fooTestObj.queen = "somebody" // => 
+fooTestObj.princess = "someone" //
+
+let barTestObj = Object.seal(testObj)
+barTestObj.queen = "freya" // =>
+barTestObj.princess = "someone" // =>
 
 testObj.hasOwnProperty("king") // =>
